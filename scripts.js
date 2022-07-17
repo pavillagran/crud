4f31dcb4414fa291c603a9d5c86d6623850cb936
pavillagran <105095873+pavillagran@users.noreply.github.com>
2022-07-16 22:38:50 -0400

    html css y crear objeto, lectura y persistencia de localstorage

---
+++ scripts.js
@@ -0,0 +1,51 @@
+// VARIABLES
+const formularioUI = document.querySelector('#formulario');
+const listaTareasUI = document.querySelector('#lista');
+
+let arrayLista = [];   // array vacio
+
+
+//FUNCIONES
+const CrearItem = (actividad) => {
+  let item ={
+  actividad: actividad,
+  descripcion: description,
+  estado: false
+}
+
+arrayLista.push(item);
+//console.log(arrayLista)
+return item;
+}
+
+// guardar db
+const GuardarDB = () => {
+  localStorage.setItem('rutina', JSON.stringify(arrayLista));
+  PintarDB();
+}
+
+const PintarDB  = () =>{
+  listaTareasUI.innerHTML = '';
+  arrayLista = JSON.parse(localStorage.getItem('rutina'));
+  // console.log(arrayLista);
+  if(arrayLista === null){
+    arrayLista = [];    // se vacia si esta nulo
+  }else{
+    arrayLista.forEach(element => {
+      //console.log(element);  ver si es persistente
+      listaTareasUI.innerHTML +=    `<li>${element.actividad} </li><button  id="btn-editar">Editar</button><button id="btn-borrar">Eliminar</button>`
+    });
+  }
+}
+//EVENT LISTENERS 
+formularioUI.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let actividadUI = document.querySelector('#actividad').value;
+  console.log(actividadUI);
+ 
+  CrearItem(actividadUI); 
+  GuardarDB();          
+  formularioUI.reset();            // resetea el formulario cada vez que apreta el btn
+});
+
+document.addEventListener('DOMContentLoaded', PintarDB)
